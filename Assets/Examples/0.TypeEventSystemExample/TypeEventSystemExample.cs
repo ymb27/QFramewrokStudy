8526fec0c1f5bcb5c025ed83816088e73242e848
yinmb <yinmb@aeroht.com>
2023-01-30 19:46:35 +0800

    完成架构篇学习

---
+++ Assets/Examples/0.TypeEventSystemExample/TypeEventSystemExample.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace QFramework.Example
+{
+    public interface IEvent{}
+
+    public struct EventA : IEvent
+        {
+            public int Count;
+        }
+
+    public struct EventB : IEvent
+    {
+        public int Count;
+    }
+
+    public class TypeEventSystemExample : MonoBehaviour,IOnEvent<EventA>, IOnEvent<EventB>
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.RegisterEvent<EventA>().UnRegisterWhenGameObjectDestroyed(gameObject);
+            this.RegisterEvent<EventB>().UnRegisterWhenGameObjectDestroyed(gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                TypeEventSystem.Global.Send(new EventA()
+                {
+                    Count = 10
+                });
+            }
+            else if(Input.GetMouseButtonDown(1))
+            {
+                TypeEventSystem.Global.Send(new EventB()
+                {
+                    Count = -10
+                });
+            }
+        }
+
+        public void OnEvent(EventA e)
+        {
+            Debug.Log(e.Count);
+        }
+
+        public void OnEvent(EventB e)
+        {
+            Debug.Log(e.Count);
+        }
+    }
+}
---
+++ Assets/Examples/1.EasyEventExample/EasyEventExample.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QFramework.Example
+{
+    public class EasyEventExample : MonoBehaviour
+    {
+        public EasyEvent EventA = new EasyEvent();
+        public EasyEvent<int> OnCountChangedEvent = new EasyEvent<int>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            EventA.Register(() =>
+            {
+                Debug.Log("EasyEvent Received");
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+
+            OnCountChangedEvent.Register(count =>
+            {
+                Debug.Log(count);
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+
+            EasyEvents.Register<EasyEvent<int, string, int>>();
+            EasyEvents.Get<EasyEvent<int, string, int>>().Register((count, name, value) =>
+            {
+                Debug.Log(count + "," + name + "," + value);
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                EventA.Trigger();
+                OnCountChangedEvent.Trigger(1);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                EasyEvents.Get<EasyEvent<int, string, int>>().Trigger(10, "test", 10);
+            }
+        }
+    }
+}
